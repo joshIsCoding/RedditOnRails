201481b7865d17dacd1124463bd0804f05b00d27
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 21:10:52 +0000

    Add #destroy action with appropriate before_action filters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: :new
+  before_action :ensure_login, only: :destroy
+  
   def new
   end
 
   def destroy
+    logout!
+    redirect_back(fallback_location: :root)
   end
 end
