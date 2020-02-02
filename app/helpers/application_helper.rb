36d1955721337e733da21b2a87e6701c87c2a028
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 19:19:11 +0000

    Add csrf_token string generator for forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def csrf_token
+    form_string = '<input type="hidden" name="authenticity_token" '
+    form_string << "value=\"#{form_authenticity_token}\"> "
+    form_string.html_safe
+  end
 end
