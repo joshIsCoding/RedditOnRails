7a71e3043ba74b50b2fab6a5401034cca4ecbf59
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 16:42:41 +0000

    Fix subject label mismatch; alter password validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) { User.create(username: "main_user", password: "test_pass")}
+  subject(:main_user) { User.create(username: "main_user", password: "test_pass")}
   describe "Validations" do
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:password_digest) }
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
       is_expected.to validate_length_of(:password).
       is_at_least(5)
     end
-    it { is_expected.to allow_value(nil).for(:password) }
+    it { is_expected.to validate_presence_of(:password).allow_nil }
   end
 
   describe "Methods" do
