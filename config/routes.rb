f23601591e74eb891d41ce04009428004f8319b5
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 17:38:46 +0000

    Rename #new routes for Users and Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:create]
+  resource :session, only: [:create, :destroy]
+  get "register", to: "users#new"
+  get "login", to: "sessions#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
