483078a057b520f8aa962d2bb1bade39b548cafb
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 16:00:38 +0000

    Add user methods specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,43 @@ RSpec.describe User, type: :model do
     it { is_expected.to allow_value(nil).for(:password) }
   end
 
-  
+  describe "Methods" do
+    describe "has_password?(password)" do
+      context "when the correct password is provided" do
+        it "returns true" do
+          expect(main_user.has_password?(main_user.password)).to be true
+        end
+      end
+      context "when an incorrect password is provided" do
+        it "returns false" do
+          expect(main_user.has_password?("incorrect_pass")).to be false
+        end
+      end
+    end
+
+    describe "::find_by_credentials(username, password)" do
+      context "when an existing user's credentials are provided" do
+        it "returns the user" do
+          expect(User.find_by_credentials(main_user.username, main_user.password)).
+          to eq(main_user)
+        end
+      end
+      context "when partially correct or non-existant credentials are provided" do
+        it "returns nil" do
+          expect(User.find_by_credentials(main_user.username, "not_pass")).
+          to be nil
+          expect(User.find_by_credentials("no_user", "not_pass")).to be nil
+        end
+      end
+    end
+
+    describe "#reset_session_token!" do
+      it "changes the session_token in the database" do
+        old_token = main_user.session_token
+        main_user.reset_session_token!
+        main_user.valid?
+        expect(main_user.session_token).to_not eq(old_token)
+      end
+    end
+  end
 end
