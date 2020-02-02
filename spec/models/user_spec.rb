3ad7d883499ba9ee81ccb64739db83ce43af09aa
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 15:53:38 +0000

    Write validations specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { User.create(username: "main_user", password: "test_pass")}
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:password_digest) }
+    it { is_expected.to validate_presence_of(:session_token) }
+
+    it { is_expected.to validate_uniqueness_of(:username) }
+    it do 
+      is_expected.to validate_length_of(:password).
+      is_at_least(5)
+    end
+    it { is_expected.to allow_value(nil).for(:password) }
+  end
+
+  
 end
