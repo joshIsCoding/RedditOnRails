5a6a0829950a6246bd6a97d9d7ec5879ba235b9f
joshIsCoding <yes_you_do@hotmail.com>
2020-02-02 19:19:50 +0000

    Add create action and before_action rules

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login
+  before_action :already_logged_in
   def new
   end
 
   def create
+    @new_user = User.new(user_params)
+    if @new_user.save
+      login_user!(@new_user)
+      redirect_to :root
+    else
+      flash.now[:errors] = @new_user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
