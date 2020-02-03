6a46ab05f36c03b8f226cbdea5f0fc082280905f
joshIsCoding <yes_you_do@hotmail.com>
2020-02-03 20:24:35 +0000

    Add all standard routes for subs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :users, only: [:create]
   resource :session, only: [:create, :destroy]
+  resources :subs
   get "register", to: "users#new"
   get "login", to: "sessions#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
