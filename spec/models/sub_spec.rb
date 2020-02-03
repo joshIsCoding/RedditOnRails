cfaab977b12155ca1c3e2302a601734ff3e46484
joshIsCoding <yes_you_do@hotmail.com>
2020-02-03 20:18:22 +0000

    Add reference records for validations tests and add foreign_key spec to associations test

--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Sub, type: :model do
-  
+  let(:user) { User.new(username: "Tester", password: "testing") }
+  subject(:sub) do 
+    Sub.new(
+      title: "Test Sub", 
+      description: "The best sub",
+      moderator: user
+    )
+  end
   describe "Validations" do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
@@ -9,7 +16,7 @@ RSpec.describe Sub, type: :model do
   end
 
   describe "Associations" do
-    it { is_expected.to belong_to(:moderator).class_name('User') }
+    it { is_expected.to belong_to(:moderator).with_foreign_key('user_id') }
   end
 
 end
