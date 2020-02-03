8b8e7bf5f89d69d6e097f459b4c5be7bc3d88c74
joshIsCoding <yes_you_do@hotmail.com>
2020-02-03 12:57:15 +0000

    Add create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,31 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: :new
+  skip_before_action :require_login, only: [:new, :create]
   before_action :ensure_login, only: :destroy
-  
+
   def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      session_params[:username], 
+      session_params[:password]
+    )
+    if @user
+      login_user!(@user)
+      redirect_to :root
+    else
+      flash.now[:errors] = ["Invalid credentials"]
+      render :new
+    end
   end
 
   def destroy
     logout!
     redirect_back(fallback_location: :root)
+  end
+
+  private
+  def session_params
+    params.require(:user)
   end
 end
