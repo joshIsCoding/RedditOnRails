fbe94b845658256b79459c11364be5d4c98b83b7
joshIsCoding <yes_you_do@hotmail.com>
2020-02-04 22:48:44 +0000

    Add name validations

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,6 +1,8 @@
 class Sub < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
+  validate :name_has_no_spaces_or_punctuation
   
   belongs_to(
     :moderator, 
@@ -8,5 +10,12 @@ class Sub < ApplicationRecord
     foreign_key: :user_id,
     primary_key: :id
   )
-
+  private
+  def name_has_no_spaces_or_punctuation
+    if self.name
+      unless self.name.match?(/^\w+$/)
+        errors[:name] << "cannot contain spaces or punctuation"
+      end
+    end
+  end
 end
