783ad88a77d0407ec821fd46bf15a3c591c941b9
joshIsCoding <yes_you_do@hotmail.com>
2020-02-04 22:47:54 +0000

    Add name validation specs

--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -4,15 +4,26 @@ RSpec.describe Sub, type: :model do
   let(:user) { User.new(username: "Tester", password: "testing") }
   subject(:sub) do 
     Sub.new(
+      name: "Test",
       title: "Test Sub", 
       description: "The best sub",
       moderator: user
     )
   end
   describe "Validations" do
+    it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
     it { is_expected.to validate_uniqueness_of(:title) }
+
+    it do 
+      is_expected.to_not allow_values("invalid name", "Another Invalid name").
+      for(:name)
+    end
+
+    it do 
+      is_expected.to allow_values("ValidName", "AnotherValidName").for(:name)
+    end
   end
 
   describe "Associations" do
