7146da0633d8e82ed835f9763772edf083cf5527
joshIsCoding <yes_you_do@hotmail.com>
2020-02-04 21:18:00 +0000

    Refactor login and registration actions with user auth helper methods

--- spec/system/user_auth_spec.rb
+++ spec/system/user_auth_spec.rb
@@ -1,22 +1,19 @@
+require 'support/auth_helper'
 require 'rails_helper'
 
 RSpec.describe "User Authentication", type: :system do
-  let(:new_user) { User.new(username: "new_user", password: "test_pass")}
+  let(:new_username) { "new_user" }
   let(:existing_user) { User.create!(username: "existing_user", password: "test_pass")}
   describe "User Registration" do
-    before(:each) { visit(register_path) }
 
     it "has a registration page" do
+      visit(register_path)
       expect(page).to have_selector("form.user")
     end
 
     context "when valid credentials are provided" do
       before(:each) do
-        within 'form.user' do
-          fill_in 'user_username', with: new_user.username 
-          fill_in 'user_password', with: new_user.password
-          click_button 'SIGN UP'
-        end
+        register(new_username)
       end
       
       it "redirects to the home page" do
@@ -24,17 +21,13 @@ RSpec.describe "User Authentication", type: :system do
       end
       
       it "shows the user's username in the header bar" do
-        expect(find("header h6")).to have_content(new_user.username)
+        expect(find("header h6")).to have_content(new_username)
       end
     end
     
     context "when invalid credentials are submitted" do
       it "refreshes the form with an error" do
-        within 'form.user' do
-          fill_in 'user_username', with: new_user.username 
-          fill_in 'user_password', with: "test"
-          click_button 'SIGN UP'
-        end
+        register(new_username, "fail")
         expect(page).to have_current_path(users_path)
         expect(page).to have_content("too short")
       end
@@ -42,19 +35,15 @@ RSpec.describe "User Authentication", type: :system do
   end
 
   describe "User Login" do
-    before(:each) { visit(login_path) }
 
     it "has a login page" do
+      visit(login_path)
       expect(page).to have_selector("form.user")
     end
 
     context "when valid credentials are provided" do
       before(:each) do
-        within 'form.user' do
-          fill_in 'user_username', with: existing_user.username 
-          fill_in 'user_password', with: existing_user.password
-          click_button 'SIGN IN'
-        end
+        login(existing_user)
       end
 
       it "redirects to the home page" do
@@ -68,11 +57,7 @@ RSpec.describe "User Authentication", type: :system do
     
     context "when invalid credentials are submitted" do
       it "refreshes the form with an error" do
-        within 'form.user' do
-          fill_in 'user_username', with: existing_user.username 
-          fill_in 'user_password', with: "not_right_pass"
-          click_button 'SIGN IN'
-        end
+        login(existing_user, "not_right_pass")
         expect(page).to have_current_path(session_path)
         expect(page).to have_content("Invalid credentials")
       end
