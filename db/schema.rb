4bbadd99b74c81d2c163b7c01548bac4a3b2190a
joshIsCoding <yes_you_do@hotmail.com>
2020-02-05 16:50:23 +0000

    Generate and run migration to create posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_221646) do
+ActiveRecord::Schema.define(version: 2020_02_05_163150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.bigint "sub_id", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["sub_id"], name: "index_posts_on_sub_id"
+    t.index ["title"], name: "index_posts_on_title"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "title", null: false
@@ -37,5 +50,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_221646) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "subs"
+  add_foreign_key "posts", "users", column: "author_id"
   add_foreign_key "subs", "users"
 end
