33ccab198ff15b4aa59bb301a13001ce0e38d5a1
joshIsCoding <yes_you_do@hotmail.com>
2020-02-05 12:52:50 +0000

    Add new and create actions

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,9 +1,18 @@
 class SubsController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
   def new
+    @sub = Sub.new
   end
 
   def create
+    @sub = Sub.new(sub_params)
+    @sub.moderator = current_user
+    if @sub.save
+      redirect_to sub_url(@sub)
+    else
+      flash.now[:errors] = @sub.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +30,10 @@ class SubsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def sub_params
+    params.require(:sub).permit(:name, :title, :description)
   end
 end
