cef0063709433390c6b857ee9fec82201ba59b65
joshIsCoding <yes_you_do@hotmail.com>
2020-02-06 11:45:10 +0000

    Add posts resources including sub-nested #new route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :users, only: [:create]
   resource :session, only: [:create, :destroy]
-  resources :subs
+  resources :subs do
+    resources :posts, only: :new
+  end
+  resources :posts, except: [:index, :new]
   get "register", to: "users#new"
   get "login", to: "sessions#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
