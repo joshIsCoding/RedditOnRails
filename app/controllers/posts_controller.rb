246d9abda5b7ffc2bc378eb2fa5e9b7d92048f14
joshIsCoding <yes_you_do@hotmail.com>
2020-02-06 22:28:42 +0000

    Add #new, #create and #show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,22 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find_by_id(params[:id])
   end
 
   def new
+    @post = Post.new
+    @post.sub = Sub.find_by_id(params[:id])
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to post_url(@post)
+    else
+      flash.now[:errors] = @post.errors.full_messages
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +26,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content, :sub_id)
   end
 end
