eb76ae8282e73e6ac63e28a64f0dd9a6783226bc
joshIsCoding <yes_you_do@hotmail.com>
2020-02-08 00:04:19 +0000

    Add validation and association specs

---
+++ spec/models/post_sub_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe PostSub, type: :model do
+  describe "Validations" do
+    it do
+      post_sub = PostSub.create!(
+        sub: Sub.create!(
+          name: "test",
+          title: "test",
+          description: "k",
+          moderator: User.create!(username: "Test", password: "password")
+        ),
+        post: Post.create!( title: "test", sub: Sub.first, author: User.first )
+      )
+
+      is_expected.to validate_uniqueness_of(:sub_id).scoped_to(:post_id)
+    end  
+  end
+  describe "Associations" do
+    it { is_expected.to belong_to(:post) }
+    it { is_expected.to belong_to(:sub) }
+  end
+end
