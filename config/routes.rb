68899feff081dea14e21c1faf19bc8866ed02254
joshIsCoding <yes_you_do@hotmail.com>
2020-02-24 23:49:31 +0000

    Add post_ubs destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :posts, only: :new
   end
   resources :posts, except: [:index, :new]
+  resources :post_subs, only: [:destroy]
   get "register", to: "users#new"
   get "login", to: "sessions#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
