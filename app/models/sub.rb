88b3c8865f787a4d70881c5864c022bfbf97da9c
joshIsCoding <yes_you_do@hotmail.com>
2020-02-24 20:13:06 +0000

    Add destroy dependency to :post_subs and remove the dependency from :posts

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -4,8 +4,8 @@ class Sub < ApplicationRecord
   validates :description, presence: true
   validate :name_has_no_spaces_or_punctuation
   
-  has_many :post_subs, inverse_of: :sub
-  has_many :posts, through: :post_subs, dependent: :destroy do
+  has_many :post_subs, inverse_of: :sub, dependent: :destroy
+  has_many :posts, through: :post_subs do
     def sort_created_at
        order("posts.created_at DESC")
     end
