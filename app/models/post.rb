3e2e8abae292940581c13fe0a5171b5b29011e60
joshIsCoding <yes_you_do@hotmail.com>
2020-02-26 22:38:50 +0000

    Add comments association

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,8 @@ class Post < ApplicationRecord
   
   has_many :post_subs, dependent: :destroy, inverse_of: :post
   has_many :subs, through: :post_subs
-
+  
+  has_many :comments, dependent: :destroy
   validates :subs, presence: true
   belongs_to :author, class_name: "User"
 
