4f40db52868a5743021311610558ec2a34556ab0
joshIsCoding <yes_you_do@hotmail.com>
2020-02-27 20:25:19 +0000

    Add :with_authors and :sort_created scopes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment < ApplicationRecord
   validates :contents, presence: true
   
-  belongs_to :author, class_name: "User", dependent: :destroy
-  belongs_to :post, dependent: :destroy
+  belongs_to :author, class_name: "User"
+  belongs_to :post
+  scope :with_authors, -> do
+    select("comments.*, users.username AS \"author_name\"")
+    .joins(:author)
+  end
+  scope :sort_created, -> { order(created_at: :desc) }
 end
