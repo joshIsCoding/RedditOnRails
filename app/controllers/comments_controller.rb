9c64af8893712650ac0ff19e563dcd4f2f197b3a
joshIsCoding <yes_you_do@hotmail.com>
2020-02-27 20:44:45 +0000

    Add before_action filter to ensure user is author or relevant sub mod before comment deletion

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :find_and_authenticate_comment, only: :destroy
   def create
     @comment = Comment.new(params.require(:comment).permit(:contents, :post_id))
     @comment.author = current_user
@@ -12,8 +13,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find_by_id(params[:id])
     flash[:errors] = @comment.errors.full_messages unless @comment.destroy
     redirect_back(fallback_location: @comment.post)
+  end
+
+  private
+  def find_and_authenticate_comment
+    @comment = Comment.find_by_id(params[:id])
+    unless ( @comment.author == current_user || 
+      @comment.post.sub_mods.include?(current_user) 
+    )
+      redirect_back(fallback_location: @comment.post)
+    end
   end
 end
