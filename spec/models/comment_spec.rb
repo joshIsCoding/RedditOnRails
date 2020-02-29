69a110cc9236546dbc995531ea80956ef4bb9814
joshIsCoding <yes_you_do@hotmail.com>
2020-02-29 16:21:17 +0000

    Add parent and child comment association expectations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,5 +8,7 @@ RSpec.describe Comment, type: :model do
   describe "Associations" do
     it { is_expected.to belong_to(:post) }
     it { is_expected.to belong_to(:author) }
+    it { is_expected.to belong_to(:parent_comment).optional }
+    it { is_expected.to have_many(:child_comments) }
   end
 end
