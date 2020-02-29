34dcf8550481b48cd8dce0fd4c3cb1ecb42a98bb
joshIsCoding <yes_you_do@hotmail.com>
2020-02-29 16:21:23 +0000

    Add parent and child comment associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,9 @@ class Comment < ApplicationRecord
   
   belongs_to :author, class_name: "User"
   belongs_to :post
+  belongs_to :parent_comment, class_name: "Comment", required: false
+
+  has_many :child_comments, class_name: "Comment", foreign_key: "parent_comment_id"
   scope :with_authors, -> do
     select("comments.*, users.username AS \"author_name\"")
     .joins(:author)
