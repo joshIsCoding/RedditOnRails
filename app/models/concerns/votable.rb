a522f1e1d9726652f35a3b60c45559ec4ac8f31f
joshIsCoding <yes_you_do@hotmail.com>
2020-03-08 23:42:43 +0000

    Add destroy dependency to :votes; update :sort_by_votes to treat voteless records as having a vote score of 0

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -4,9 +4,9 @@ module Votable
   extend ActiveSupport::Concern
 
   included do
-    has_many :votes, as: :votable
+    has_many :votes, as: :votable, dependent: :destroy
     scope :sort_by_votes, -> do 
-      select("posts.*, SUM(votes.value) AS vote_sum")
+      select("posts.*, COALESCE(SUM(votes.value), 0) AS vote_sum")
       .left_joins(:votes)
       .order(vote_sum: :desc)
       .group("posts.id")
