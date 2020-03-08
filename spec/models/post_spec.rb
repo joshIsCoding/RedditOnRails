d88a43125f233ee582a7e1bccad813b6bcb8e042
joshIsCoding <yes_you_do@hotmail.com>
2020-03-08 23:57:12 +0000

    Add sort_by_votes scope specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -25,6 +25,50 @@ RSpec.describe Post, type: :model do
     it { is_expected.to have_many(:post_subs).dependent(:destroy) }
     it { is_expected.to have_many(:subs) }
     it { is_expected.to have_many(:comments).dependent(:destroy) }
+    it { is_expected.to have_many(:votes).dependent(:destroy) }
     it { is_expected.to belong_to(:author) }
+  end
+
+  describe "Votes and Related Methods" do
+    let(:users) do
+      u = []
+      3.times {|i| u << User.create!(username: "user_#{i+1}", password: "password")}
+      u
+    end
+    let(:sub) do
+      Sub.create!(
+          name: "Main",
+          title: "Main", 
+          description: "For talking about everything.",
+          moderator: users.first
+        )
+    end
+    let(:posts) do 
+      p = []
+      3.times do |i|
+        p << Post.create!(
+          title: "Post_#{i+1}",
+          content: "Post contents",
+          subs: [sub],
+          author: users.first
+        )
+      end
+      p
+    end
+    let!(:votes) do
+      posts.each_with_index do |post, i|
+        i.times { |j| Vote.create!(votable: posts[i], voter: users[j]) }
+      end
+    end
+    subject(:sorted_posts) { sub.posts.sort_by_votes }
+    describe "#sort_by_votes scope" do
+      it "should return posts sorted by their total vote score" do
+        expect(sorted_posts).to eq(posts.reverse)
+      end
+      it "should return posts with a score psuedo-attribute" do
+        expect(sorted_posts.first.vote_sum).to eq(2)
+        expect(sorted_posts.last.vote_sum).to eq(0)
+      end
+    end
   end
 end
