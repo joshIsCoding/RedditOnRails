038c2b89522ae40cf489879cc93f4a816449f06c
joshIsCoding <yes_you_do@hotmail.com>
2020-03-09 00:19:17 +0000

    Add :sort_by_votes specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,4 +12,54 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_many(:child_comments) }
     it { is_expected.to have_many(:votes).dependent(:destroy) }
   end
+
+  describe "Votes and Related Methods" do
+    let(:users) do
+      u = []
+      3.times {|i| u << User.create!(username: "user_#{i+1}", password: "password")}
+      u
+    end
+    let(:sub) do
+      Sub.create!(
+          name: "Main",
+          title: "Main", 
+          description: "For talking about everything.",
+          moderator: users.first
+        )
+    end
+    let(:post) do
+      Post.create!(
+        title: "Main Post",
+        content: "User's Post",
+        subs: [sub],
+        author: users.first
+      )
+    end
+    let(:comments) do 
+      c = []
+      3.times do |i|
+        c << Comment.create!(
+          post: post,
+          contents: "Great comment #{i}",
+          author: users.first
+        )
+      end
+      c
+    end
+    let!(:votes) do
+      comments.each_with_index do |post, i|
+        i.times { |j| Vote.create!(votable: comments[i], voter: users[j]) }
+      end
+    end
+    subject(:sorted_comments) { post.comments.sort_by_votes }
+    describe "#sort_by_votes scope" do
+      it "should return comments sorted by their total vote score" do
+        expect(sorted_comments).to eq(comments.reverse)
+      end
+      it "should return comments with a score psuedo-attribute" do
+        expect(sorted_comments.first.vote_sum).to eq(2)
+        expect(sorted_comments.last.vote_sum).to eq(0)
+      end
+    end
+  end
 end
