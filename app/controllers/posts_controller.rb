ef339f72918dc4c56ff87dc1ba65e6b313c17ae6
joshIsCoding <yes_you_do@hotmail.com>
2020-04-13 22:31:18 +0100

    Add #up- and down-#vote controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
+  skip_before_action :require_login, only: :show
+  before_action :find_post, except: [:new, :create]
   before_action :ensure_user_is_author, only: [:edit, :update]
   before_action :ensure_user_has_authority, only: :destroy
-  skip_before_action :require_login, only: :show
+  
   def show
-    get_post_from_params
     @comments = @post.comments_by_parent_id
   end
 
@@ -39,25 +40,31 @@ class PostsController < ApplicationController
     destroy_post(@post)
   end
 
+  def upvote
+    vote :up, @post
+  end
+
+  def downvote
+    vote :down, @post
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content, sub_ids:[])
   end
 
-  def get_post_from_params
+  def find_post
     @post = Post.find_by_id(params[:id])
     render_not_found unless @post
   end
 
   def ensure_user_is_author
-    get_post_from_params
     unless current_user == @post.author
       redirect_back(fallback_location: post_url(@post))
     end
   end
 
   def ensure_user_has_authority
-    get_post_from_params
     unless current_user == @post.author || (
       @post.subs.count == 1 && current_user == @post.subs.first.moderator
     )
