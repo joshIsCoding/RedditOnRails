5f6062ceaef6fcb1d1ff528b8dad97ba9352cda7
joshIsCoding <yes_you_do@hotmail.com>
2020-04-13 22:40:22 +0100

    Add missing required fields and subfactory to generate related posts

--- spec/factories/subs.rb
+++ spec/factories/subs.rb
@@ -1,7 +1,17 @@
 FactoryBot.define do
   factory :sub do
-    sequence(:title) { |n| "sub_#{n}" }
-    description { "MyText" }
+    sequence(:title) { |n| "Sub #{n}" }
+    sequence(:name) { |n| "sub#{n}" }
+    description { "Text relating to #{name}" }
     association :moderator, factory: :user
+    factory :sub_with_posts do
+      transient do
+        posts_count { 5 }
+      end  
+      after :create do |sub, evaluator|
+        create_list(:post, evaluator.posts_count, subs: [sub] )
+      end
+    end
+    
   end
 end
