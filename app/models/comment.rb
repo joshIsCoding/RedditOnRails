dad7c1842c1bf438333485c6e4577272e1ab1d1c
joshIsCoding <yes_you_do@hotmail.com>
2020-05-10 16:26:54 +0100

    Add temporary rudimentary solution to avoid pg grouping error using ::with_authors and ::with_votes together

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,9 +8,17 @@ class Comment < ApplicationRecord
 
   has_many :child_comments, class_name: "Comment", foreign_key: "parent_comment_id"
   scope :with_authors, -> do
-    select("comments.*, users.username AS \"author_name\"")
+    select("comments.*, users.username AS author_name")
     .joins(:author)
   end
+
+  scope :with_votes_and_authors, -> do
+    with_authors
+    .select( "comments.*, COALESCE(SUM(votes.value), 0) AS vote_sum" )
+    .left_joins(:votes)
+    .group("#{self.table_name}.id, users.username")
+  end
+
   scope :top_level, -> { where(parent_comment_id: nil) }
   scope :sort_created, -> { order(created_at: :desc) }
 end
