e329b6e99315a3eb685c5d936c2118194a398e7d
joshIsCoding <yes_you_do@hotmail.com>
2020-05-17 18:53:47 +0100

    Add whitespacing

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -3,20 +3,23 @@ class Sub < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
   validate :name_has_no_spaces_or_punctuation
-  
+
   has_many :post_subs, inverse_of: :sub, dependent: :destroy
   has_many :posts, through: :post_subs do
     def sort_created_at
        order("posts.created_at DESC")
     end
   end
+
   belongs_to(
     :moderator, 
     class_name: 'User', 
     foreign_key: :user_id,
     primary_key: :id
   )
+
   private
+
   def name_has_no_spaces_or_punctuation
     if self.name
       unless self.name.match?(/^\w+$/)
