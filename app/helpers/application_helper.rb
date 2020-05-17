307eb5a401637687adaec2595c948518caea61c6
joshIsCoding <yes_you_do@hotmail.com>
2020-05-17 18:52:32 +0100

    Add hide_vote_buttons? helper to remove logic from views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     form_string << "value=\"#{form_authenticity_token}\"> "
     form_string.html_safe
   end
+
+  def hide_vote_buttons?( target )
+    target.author == current_user || target.votes.where( voter: current_user ).any?
+  end
 end
